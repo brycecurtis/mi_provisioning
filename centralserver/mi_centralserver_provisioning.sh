ccb390c9de991fc183527af771f50f6eba301f9c
ASHER C. JOHNSON <asherjohnson@us.ibm.com>
2017-02-15 09:46:32 -0600

    Updating to download latest docker.

--- centralserver/mi_centralserver_provisioning.sh
+++ centralserver/mi_centralserver_provisioning.sh
@@ -2,7 +2,7 @@
 # Update System 
 sudo apt-get -y update
 # Install Build Tools
-sudo apt-get -y -f install vim make git lxc curl
+sudo apt-get -y -f install vim make git curl
 
 # Prevent root login via SSH
 sed -i -e "s/PermitRootLogin [y|Y]es/PermitRootLogin without-password/g" /etc/ssh/sshd_config 
@@ -10,13 +10,7 @@ sed -i -e "s/PasswordAuthentication [y|Y]es/PasswordAuthentication no/g" /etc/ss
 service ssh restart 
 
 # Install Latest Ubuntu Trusty Docker Package
-sudo apt-get -y install linux-image-extra-$(uname -r)
-sudo sh -c "wget -qO- https://get.docker.io/gpg | apt-key add -"
-sudo sh -c "echo deb http://get.docker.io/ubuntu docker main\ > /etc/apt/sources.list.d/docker.list"
-sudo apt-get update
-sudo apt-get -y install lxc-docker
-echo 'DOCKER_OPTS="-e lxc"' >> /etc/default/docker
-sudo service docker restart
+curl -sSL https://get.docker.com/ | sh
 
 # Install the startup script
 git clone https://github.com/asherjohnsonibm/mi_provisioning/ /usr/share/mi_startup
